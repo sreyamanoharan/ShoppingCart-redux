7d9d5a71740b5a474a483548d08b0f96766de983
sreyamanoharan <sreyampk@gmail.com>
2024-10-11 10:55:41 +0530

    last commit

--- src/Components/CartDetails.jsx
+++ src/Components/CartDetails.jsx
@@ -1,22 +1,46 @@
 import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import {removeItem} from '../store/slice'
 
 const CartDetails = () => {
 
+  const dispatch = useDispatch()
 
-    const cartDetail=useSelector((state)=>state.productDetails.cartItems)
-    console.log(cartDetail);
-    
-  
+  const cartDetail = useSelector((state) => state.productDetails.cartItems)
+  console.log(cartDetail);
+
+  const [localQuantities, setLocalQuantities] = useState(
+    cartDetail.map((item) => item.quantity)
+  );
+
+  const inc = (index) => {
+    const newQuantities = [...localQuantities];
+    newQuantities[index] += 1;
+    setLocalQuantities(newQuantities);
+  }
+
+  const dec = (index) => {
+    const newQuantities = [...localQuantities];
+    newQuantities[index] -= 1;
+    setLocalQuantities(newQuantities);
+  }
+
+  const removeCartItem = (i) => {
+    dispatch(removeItem(i))
+  }
+
+
 
   return (
     <div>
-  {cartDetail.map((cart,i)=>(
-    <li>
-        <name>{cart.strCategory}</name>
-        <img src={cart.strCategoryThumb} alt="" /></li>
-  ))}
-
+      {cartDetail.map((cart, i) => (
+        <li>
+          <name>{cart.strCategory}</name>
+          <img src={cart.strCategoryThumb} alt="" /><br />
+          <button onClick={() => dec(i)}>-</button><p>count: {localQuantities[i]}</p><button onClick={() => inc(i)}>+</button>
+          <button onClick={() => removeCartItem(cart.idCategory)}>Remove</button>
+        </li>
+      ))}
     </div>
   )
 }
--- src/store/slice.js
+++ src/store/slice.js
@@ -13,12 +13,23 @@ const productList=createSlice({
         },
         addToCart:(state,action)=>{
             const item = action.payload;
-            console.log('cart is here',action.payload);
-            state.cartItems.push({ ...item, quantity: 1 });
+            const existingItem=state.cartItems.find(cartItem=>cartItem.idCategory===item.idCategory)
+            if(existingItem){
+                existingItem.quantity+=1
+            }else{
+                console.log('cart is here',action.payload);
+                state.cartItems.push({ ...item, quantity: 1 });
+            }
+          
             
+        },
+        removeItem:(state,action)=>{
+            
+            state.cartItems=state.cartItems.filter((cartItem)=>cartItem.idCategory!==action.payload)
+
         }
     }
 })
 
-export const {addItem,addToCart}=productList.actions
+export const {addItem,addToCart,removeItem}=productList.actions
 export default productList.reducer
